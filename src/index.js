60e6961382ef106028b72e95d23fe0a21475aada
Hyun <seizethedayee@gmail.com>
2025-12-04 11:30:49 +0900

    second commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // computes a factorial of a given positive integer
 function factorial(n) {
 
-    if( n < 0 ) {
+    /*if( n < 0 ) {
         throw new Error('n must be a positive integer');
-    }
+    }*/
 
 
     if( n === 0 ) {
